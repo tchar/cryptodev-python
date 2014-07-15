445c8040b5111150eab7494037c34f63e7352cd8
tchar <tilemachos.charalampous@gmail.com>
2014-07-15 23:00:03 +0300

    Added python script to run all tests.

---
+++ tests/run-tests.py
@@ -0,0 +1,22 @@
+from os import system, path,listdir
+tests = ["async_cipher.py", "async_hmac.py", "async_speed.py",
+			"cipher-aead-srtp.py", "cipher-aead.py", "cipher-gcm.py",
+			"cipher.py", "cipher_comp.py", "fullspeed.py",
+			"hash_comp.py", "hascrypt_speed.py", "hmac.py",
+			"hmac_comp.py", "sha_speed.py", "speed.py"]
+
+def main():
+	try:
+		files = [f for f in listdir('.') if path.isfile(f)]
+		for f in set(files) & set(tests):
+			print 40 * "="
+			print "Running " + f
+			print 40 * "="
+			system("python " + f)
+			print ""
+		print 40 * "="
+	except Exception, e:
+		print str(e)
+
+if __name__ == "__main__":
+	main()
